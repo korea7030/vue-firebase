d23409c55de48a82e1561388ffa87fd6184f9a24
korea7030 <korea7030@naver.com>
2020-11-01 15:17:58 +0900

    menu add and move 22

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: 'AIzaSyAqB-dLjwH0xJl8EPbrJ6fDBFh89ez6zag',
+  authDomain: 'vue-firebase-f7968.firebaseapp.com',
+  databaseURL: 'https://vue-firebase-f7968.firebaseio.com',
+  projectId: 'vue-firebase-f7968',
+  storageBucket: 'vue-firebase-f7968.appspot.com',
+  messagingSenderId: '538890483885',
+  appId: '1:538890483885:web:51067728f7971eacee2e24',
+  measurementId: 'G-P6045MEL48'
+}
